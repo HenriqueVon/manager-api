3a00521b9657adec43d3cb79614f708552af73d2
Henrique Weidebach <henriqueweidebach@gmail.com>
2026-02-01 15:10:31 +0000

    refactor: ledger controller and usecase

--- src/modules/ledger/ledger.controller.ts
+++ src/modules/ledger/ledger.controller.ts
@@ -8,7 +8,6 @@ import {
   GetByIdLedgerUseCase,
   DeleteLedgerUseCase
 } from './usecases/index';
-import { UpdateLedgerDto } from './dtos';
 
 @injectable()
 export class LedgerController{
@@ -26,53 +25,27 @@ export class LedgerController{
   ){}
 
   async create(req: Request, res: Response) {        
-    const { name, type } = req.body;
-
-    try {
-      const ledger = await this.createLedgerUseCase.execute({ name, type });
-      return res.status(201).json(ledger);
-    } catch {
-      return res.status(400).json({ message: 'Bad Request'} )
-    }
+    const ledger = await this.createLedgerUseCase.execute(req.body);
+    return res.status(201).json(ledger);
   }
 
   async list(req: Request, res: Response) {        
-    try {
-      const ledgers = await this.listLedgerUseCase.execute();
-      return res.status(200).json(ledgers);
-    } catch {
-      return res.status(400).json({ message: 'Bad Request'})
-    }
+    const ledgers = await this.listLedgerUseCase.execute();
+    return res.status(200).json(ledgers);
   }
 
-  async getById(req: Request, res: Response) {
-    try {
-      const { id } = req.params;
-      const ledger = await this.getByIdLedgerUseCase.execute(id as string);
-      return res.status(200).json(ledger);
-    } catch {
-      return res.status(400).json({ message: 'Bad Request'});
-    }
+  async getById(req: Request, res: Response) {    
+    const ledger = await this.getByIdLedgerUseCase.execute(req.params.id);
+    return res.status(200).json(ledger);
   }
 
-  async update(req: Request, res: Response) {
-    try {
-      const { id } = req.params;
-      const data : UpdateLedgerDto = req.body;
-      const ledger = await this.updateLedgerUseCase.execute(id as string, data);
-      return res.status(200).json(ledger);
-    } catch {
-      return res.status(400).json({ message: 'Bad Request'} )
-    }
+  async update(req: Request, res: Response) {    
+    const ledger = await this.updateLedgerUseCase.execute(req.params.id, req.body);
+    return res.status(200).json(ledger);
   }
 
-  async delete (req: Request, res: Response) {
-    try {
-      const { id } = req.params;
-      await this.deleteLedgerUseCase.execute(id as string);
-      return res.status(204).send();
-    } catch {
-      return res.status(400).json({ message: 'Bad Request'} );
-    }
+  async delete (req: Request, res: Response) {    
+    await this.deleteLedgerUseCase.execute(req.params.id);
+    return res.status(204).send();
   }
 }
--- src/modules/ledger/usecases/get-by-id-ledger.usecase.ts
+++ src/modules/ledger/usecases/get-by-id-ledger.usecase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 import { ILedgerRepository } from '../repositories/iledger.repository';
 import { LEDGER_REPOSITORY } from '../repositories/ledger.tokens';
 import { Ledger } from '@prisma/client';
+import { NotFoundError } from '../../../shared/errors/app-error';
 
 @injectable()
 export class GetByIdLedgerUseCase {
@@ -11,6 +12,8 @@ export class GetByIdLedgerUseCase {
   ) {}
 
   async execute(id: string) : Promise<Ledger | null> {
-    return this.ledgerRepository.findById(id);
+    const ledger = await this.ledgerRepository.findById(id);
+    if (!ledger) throw new NotFoundError();
+    return ledger;
   }
 }
