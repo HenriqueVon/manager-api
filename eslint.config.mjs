0aaeb19e8527a251a8a35dc3f7ec0e592ea4e7c6
Henrique Weidebach <henriqueweidebach@gmail.com>
2026-02-14 20:46:37 +0000

    fix: update no-unused-vars rule to ignore leading underscores in arguments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -49,7 +49,12 @@ export default [
       ],
       '@typescript-eslint/no-empty-object-type' : 'off',
       '@typescript-eslint/no-explicit-any'      : 'off',
-      "@typescript-eslint/no-unused-vars": ["warn"],      
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],      
       "@typescript-eslint/explicit-function-return-type": 'off',
       "@typescript-eslint/no-non-null-assertion": 'off'     
     }
