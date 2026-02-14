fc9319280c0cb4f47f70e3bec8239b9cd23356f8
Henrique Weidebach <henriqueweidebach@gmail.com>
2026-02-14 20:49:21 +0000

    feat: implement PrismaClientKnownRequestError to get unique keys fields

--- src/modules/ledger/repositories/iledger.repository.ts
+++ src/modules/ledger/repositories/iledger.repository.ts
@@ -4,6 +4,7 @@ import { CreateLedgerDto, UpdateLedgerDto } from '../dtos';
 export interface ILedgerRepository {
   create(data: CreateLedgerDto): Promise<Ledger>;
   findById(id: string): Promise<Ledger | null>;
+  findByName(name: string): Promise<Ledger | null>;
   findMany(filters?: Partial<Ledger>): Promise<Ledger[]>;
   update(id: string, data: UpdateLedgerDto): Promise<Ledger>;
   delete(id: string): Promise<void>;
